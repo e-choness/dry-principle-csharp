35604afa8742bd51e5d7d00beb5914e32eaac9f9
Echo Yin <echo_ybl@hotmail.com>
2024-02-02 20:35:05 -0500

    Add tests for dry library

---
+++ DryTests/UnitTestForDry.cs
@@ -0,0 +1,33 @@
+using DryFrameworkLibrary.Interfaces;
+using DryFrameworkLibrary.Models;
+using DryFrameworkLibrary.Services;
+
+namespace DryTests;
+
+public class Tests
+{
+    private IProduct game;
+    private ProductProcessor _processor;
+    
+    [SetUp]
+    public void Setup()
+    {
+        game = new Game();
+        _processor = new ProductProcessor();
+    }
+
+    [Test]
+    public void GetNameTest()
+    {
+        var title = "The Elder Scrolls";
+        var subtile = "Skyrim";
+        var transactionDate = DateTime.Now;
+
+        game.Title = title;
+        game.Subtitle = subtile;
+        game.TransactionDate = transactionDate;
+
+        var resultID =_processor.GenerateProductId(game);
+        Assert.That(true, $"TheSky{transactionDate}", resultID);
+    }
+}
